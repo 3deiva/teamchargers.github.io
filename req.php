fec066b2ba6dd87556949fd18ba5b32adbbd134b
Deepak Chandhru N <122144025+Deepakchandhru@users.noreply.github.com>
2025-12-01 18:43:05 +0530

    Change database connection to remote server
    Updated database connection details and improved error handling.

--- req.php
+++ req.php
@@ -8,9 +8,12 @@ $user = "postgres";
 $password = "5112";
 
 try {
-    // Create PDO connection
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
-    $pdo = new PDO($dsn);
+    $pdo = new PDO(
+    "pgsql:host=dpg-d4mmlbq4d50c73eq0gs0-a.oregon-postgres.render.com;port=5432;dbname=ev_8jta",
+    "ev_8jta_user",
+    "JMxhcUXabU16VLJjiyew6oxGgxJm4Boq"
+);
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Retrieve user's contact number from session
@@ -96,4 +99,5 @@ try {
         </table>
     </main>
 </body>
+
 </html>
